3b2d3c036b9de7ea05086b3d1e63c9f45a6f85f3
Shiva666666 <155778248+Shiva666666@users.noreply.github.com>
2025-04-19 21:02:57 +0530

    Update test_recommender.py

--- tests/test_recommender.py
+++ tests/test_recommender.py
@@ -3,7 +3,7 @@ from recommender import get_recommendations
 
 class TestRecommender(unittest.TestCase):
     def test_return_type(self):
-        result = get_recommendations("happy", "rf")
+        result = get_recommendations("happy", "random-forest")
         self.assertIsInstance(result, list)
 
 if __name__ == "__main__":
